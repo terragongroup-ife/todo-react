b56a2a3ff7c2e7b055fab9ba67434f644a5c95bf
Moshood Alaran <geekyshood@gmail.com>
2019-01-29 13:13:42 +0100

    added cookie to app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,18 @@
+import {bake_cookie, read_cookie} from 'sfcookies';
+
+
 const taskReducer = (state, action) => {
+    state = read_cookie('newState');
     switch(action.type){
         case "ADD_TASK":
             const newState = [...state]
             newState.push({title : action.title, id: Math.random(), content : action.content});
-            
+            bake_cookie('newState', newState)
             return newState;
             
         case "DELETE_TASK":
             const newDeleteState = state.filter((item) => { return item.id !== action.id});
+            bake_cookie('newState', newDeleteState)
             return newDeleteState;
         default: 
             return state;
